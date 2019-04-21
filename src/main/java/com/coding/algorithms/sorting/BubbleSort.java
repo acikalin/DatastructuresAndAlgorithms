c17c734d6fefedd8543430b021cd6d428ed4cee0
Acikalin, Emrah <emrahacikalin@gmail.com>
2019-04-21 23:05:46 +0300

    Add BubbleSort And InsertionSort

---
+++ src/main/java/com/coding/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.coding.algorithms.sorting;
+
+import com.coding.algorithms.sorting.utils.Util;
+
+public class BubbleSort {
+
+    public static void sort(int[] arrayToSort) {
+        Util.printArray(arrayToSort, "Initial");
+        for (int i=0;i<arrayToSort.length-1;i++){
+            boolean swapped = false;
+            for(int j=0;j<arrayToSort.length-i-1;j++){
+                if(arrayToSort[j]>arrayToSort[j+1]){
+                    Util.swap(arrayToSort,j,j+1);
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        Util.printArray(arrayToSort, "Final");
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int[] arrayToSort = {1,5,6,4,2,7,10,3,8,9};
+        BubbleSort.sort(arrayToSort);
+    }
+}
---
+++ src/main/java/com/coding/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.coding.algorithms.sorting;
+
+import com.coding.algorithms.sorting.utils.Util;
+
+public class InsertionSort {
+    public static void sort(int[] arrayToSort) {
+        Util.printArray(arrayToSort, "Initial");
+        for (int i=0;i<arrayToSort.length-1;i++){
+            for(int j=i;j>0;j--){
+                if(arrayToSort[j+1]>arrayToSort[j]){
+                    Util.swap(arrayToSort,j+1,j);
+                }
+            }
+        }
+        Util.printArray(arrayToSort, "Final");
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int[] arrayToSort = {1,5,6,4,2,7,10,3,8,9};
+        BubbleSort.sort(arrayToSort);
+    }
+}
